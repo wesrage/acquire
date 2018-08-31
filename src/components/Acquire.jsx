7bc4cd7a6d561d20e5fe5081826eabcee45d48a8
Wesley Waldner <wesrage@gmail.com>
2018-08-31 13:46:24 -0600

    Dev workflow setup

---
+++ src/components/Acquire.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { hot } from 'react-hot-loader'
+
+class Acquire extends React.Component {
+   render() {
+      return <h1>Acquire</h1>
+   }
+}
+
+export default hot(module)(Acquire)
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import { render } from 'react-dom'
+import './index.html'
+import Acquire from './components/Acquire'
+
+const target = document.getElementById('root')
+render(<Acquire />, target)
